67c6195be4f12e51b7443aa0867ef3f5c5367ce7
oleksandr zariechniev <o.zariechniev@gmail.com>
2024-11-28 22:40:45 +0100

    chore: cleanup initial files

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="container mx-auto flex min-h-svh items-center justify-center">
+      <h1 className="text-center text-4xl">Vite | React | TS | Tailwind</h1>
+    </div>
   </StrictMode>
 );
