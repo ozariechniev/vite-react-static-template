603dc36205c7e71c6649911749f85e8265ee96e1
oleksandr zariechniev <o.zariechniev@gmail.com>
2024-11-28 22:10:57 +0100

    fix: initial files formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
