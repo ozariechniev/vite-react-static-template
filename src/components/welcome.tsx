3fe71d67a09b875318f4e3e267f06c4ccc4ac345
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-01-03 08:50:39 +0100

    chore: add welcome component

---
+++ src/components/welcome.tsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+  return (
+    <div className="container mx-auto flex min-h-svh items-center justify-center">
+      <h1 className="p-4 text-center md:text-4xl">
+        Vite | React | TS | Tailwind
+      </h1>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import Welcome from '@/components/welcome.tsx';
+
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="container mx-auto flex min-h-svh items-center justify-center">
-      <h1 className="text-center text-4xl">Vite | React | TS | Tailwind</h1>
-    </div>
+    <Welcome />
   </StrictMode>
 );
