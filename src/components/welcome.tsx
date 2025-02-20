25f366a332f4609e4daa83dc59accc5a827c2f0d
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-02-20 19:52:05 +0100

    chore: update Tailwind CSS to v4

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,7 +1,7 @@
 export default function Welcome() {
   return (
     <div className="container mx-auto flex min-h-svh items-center justify-center">
-      <h1 className="p-4 text-center md:text-4xl">
+      <h1 className="rounded-2xl border-2 p-4 text-center md:text-4xl">
         Vite | React | TS | Tailwind
       </h1>
     </div>
